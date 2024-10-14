0da9ad54c7cdd2249982f7882d55230a44531884
DYKIM\kdy95 <kdy950930@gmail.com>
2024-10-14 14:13:37 +0900

    initial commit

---
+++ src/main/java/com/sparta/companyassignment2/CompanyAssignment2Application.java
@@ -0,0 +1,13 @@
+package com.sparta.companyassignment2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyAssignment2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyAssignment2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/companyassignment2/CompanyAssignment2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.companyassignment2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyAssignment2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
